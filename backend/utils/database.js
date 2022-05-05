2936b3cb8191838d019740d7e5c3fd5c4e7485fe
Rohan Ramakrishnan <65002749+rohan-ramakrishnan@users.noreply.github.com>
2022-05-04 19:53:41 -0700

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('express-practice', 'root', 'Mercsl100$', {
+const sequelize = new Sequelize('express-practice', 'root', 'PASSWORD', {
     host: 'localhost',
     dialect: 'mysql'
 })
